0d0ea7f3d838cb2e4094cd0a23d53aae637370d2
Getsov <info@vgetsov.com>
2022-10-21 21:06:58 +0300

    adding partytown for GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,15 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img", "svgo"],
